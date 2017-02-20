5af937811496f8cc99d5c5e61c1412f2411f2f34
Nathan Bryant <bryantnathanj@gmail.com>
2017-02-20 01:57:54 -0700

    movie description

--- src/MovieDetail/MovieDetail.js
+++ src/MovieDetail/MovieDetail.js
@@ -6,18 +6,22 @@ class MovieDetail extends Component {
     const {movieId} = this.props.routeParams;
     const {showData: {movies}} = this.props.route;
     const selectedMovie = movies.filter((movie)=>{
-      return movie.title == movieId;
+      return movie.title === movieId;
     })[0];
 
     const {
       title,
-      rating
+      rating,
+      description
     } = selectedMovie;
 
     return (
       <div>
         <h1>{title}</h1>
         <p>Rating: {rating}</p>
+        {description.split('\n\n').map(function(paragraph, i){
+          return <p>{paragraph}</p>;
+        })}
       </div>
     );
   }
